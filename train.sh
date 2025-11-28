2853e413aa935b667db5dc86261a7328ea187118
dandarm <daniele.darmiento@gmail.com>
2025-11-28 12:07:54 +0100

    par updates

--- train.sh
+++ train.sh
@@ -11,10 +11,12 @@ IMAGE_SIZE="384"
 HEATMAP_STRIDE="4"
 HEATMAP_SIGMA="8"
 BACKBONE="resnet18"
+
 EPOCHS="20"
 BATCH_SIZE="64"
 LR="3e-4"
-LOG_DIR="outputs/runs/exp3"
+
+LOG_DIR="outputs/runs/exp4"
 
 mkdir -p "$LOG_DIR"
 
