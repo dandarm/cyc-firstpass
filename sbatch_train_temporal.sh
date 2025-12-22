270869044111709e5c705b7e12ae7e0cc29b94af
Daniele D'Armiento <ddarmien@login02.leonardo.local>
2025-12-22 13:50:04 +0100

    leonardo training logs

--- sbatch_train_temporal.sh
+++ sbatch_train_temporal.sh
@@ -22,7 +22,7 @@ export PYTHONUNBUFFERED=1
 CONFIG="config/default.yml"
 TRAIN_CSV="manifests/train.csv"
 VAL_CSV="manifests/val.csv"
-LOG_DIR="outputs/runs/exp_x3dm_heatmapfocal_2"
+LOG_DIR="outputs/runs/exp_x3dm_heatmapfocal_BCE_3"
 mkdir -p "$LOG_DIR"
 
 TEMPORAL_T="16"
@@ -31,7 +31,7 @@ TEMPORAL_STRIDE="4"
 # Argomenti attivi per il nuovo modello 3D X3D
 BACKBONE="x3d_m"  #"x3d_xs"
 HEATMAP_NEG_MULT="0.5"
-HEATMAP_POS_MULT="1.0"
+HEATMAP_POS_MULT="1.5"
 
 #export NCCL_DEBUG=INFO
 # per debug dettagliato e gestione errori asincroni NCCL
