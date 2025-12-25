45a2201530ac41650c829f5d5623d3c33fab4cf7
dandarm <daniele.darmiento@gmail.com>
2025-12-25 01:18:33 +0100

    l_w

--- sbatch_train_temporal.sh
+++ sbatch_train_temporal.sh
@@ -23,7 +23,7 @@ export PYTHONUNBUFFERED=1
 CONFIG="config/default.yml"
 TRAIN_CSV="manifests/train.csv"
 VAL_CSV="manifests/val.csv"
-LOG_DIR="outputs/runs/exp_x3dm_dsnt_1"
+LOG_DIR="outputs/runs/exp_x3dm_dsnt_2"
 mkdir -p "$LOG_DIR"
 
 TEMPORAL_T="16"
